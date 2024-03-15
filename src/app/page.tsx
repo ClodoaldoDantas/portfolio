b02c66bb53cff8b411c7d6832f46916dafb534b2
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2024-03-15 17:48:56 -0300

    feat: remove experiences component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Header } from '@/components/header'
 import { About } from '@/partials/about'
 import { Skills } from '@/partials/skills'
 import { SocialLinks } from '@/partials/social-links'
-import { Experiences } from '@/partials/experiences'
 import styles from './page.module.scss'
 
 export default function Home() {
@@ -13,7 +12,6 @@ export default function Home() {
       <main className={styles.main}>
         <About />
         <Skills />
-        <Experiences />
         <SocialLinks />
       </main>
     </>
